4953abfeab30b01ef8b8979230a433c7fea89fb5
LAURA_MOERK <lauramoerk@hotmail.com>
2023-08-29 12:56:36 +0200

    Simple Number Game

---
+++ script.js
@@ -0,0 +1,75 @@
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+let guessCount = 1;
+let resetButton;
+
+
+
+function checkGuess() {
+  const userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = 'Tidligere gæt: ';
+  }
+
+  guesses.textContent += userGuess + ' ';
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = 'Tillykke, dit gæt var rigtigt!';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = '!!!GAME OVER!!!';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Forkert!';
+    lastResult.style.color = 'red';
+    lastResult.style.fontWeight = '600';
+
+    if(userGuess < randomNumber) {
+      lowOrHi.textContent = 'Dit sidste gæt var for lavt!' ;
+    } else if(userGuess > randomNumber) {
+      lowOrHi.textContent = 'Dit sidste gæt var for højt!';
+    }
+  }
+
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+};
+
+guessSubmit.addEventListener("click", e => {
+    checkGuess();
+  })
+
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Start new game';
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+  guessCount = 1;
+  const resetParas = document.querySelectorAll('.resultParas p');
+  for (const resetPara of resetParas) {
+    resetPara.textContent = '';
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+  lastResult.style.backgroundColor = 'black';
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
